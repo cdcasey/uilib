56fa4e08b121440326fd05c7edcacdd35a9b4536
Chris Casey <cdcasey@users.noreply.github.com>
2024-12-20 19:48:28 -0600

    Card fun

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,35 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { Button } from '@/components/ui/button.tsx';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-			<Button>SHAD TEST</Button>
+			<Card>
+				<CardHeader>
+					<div className="flex flex-auto">
+						<a href="https://vite.dev" target="_blank">
+							<img src={viteLogo} className="logo" alt="Vite logo" />
+						</a>
+						<a href="https://react.dev" target="_blank">
+							<img src={reactLogo} className="logo react" alt="React logo" />
+						</a>
+					</div>
+					<CardTitle>
+						<h1>Vite + React</h1>
+					</CardTitle>
+				</CardHeader>
+				<CardContent className="border-2 rounded m-auto w-1/2">
+					<Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
+					<p>
+						Edit <code>src/App.tsx</code> and save to test HMR
+					</p>
+				</CardContent>
+				<CardFooter>The footer</CardFooter>
+			</Card>
 		</>
 	);
 }
